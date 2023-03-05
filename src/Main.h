f66e8b0ed650dbe67ce1ee69b297d2a7d0bb15f7
DomoChip <domochip@hotmail.com>
2023-03-05 10:09:38 +0100

    Initial Commit

---
+++ src/Main.h
@@ -0,0 +1,53 @@
+#ifndef Main_h
+#define Main_h
+
+#include <arduino.h>
+
+//DomoChip Informations
+//Configuration Web Pages :
+//http://IP/
+
+#define APPLICATION1_HEADER "WirelessIzar.h"
+#define APPLICATION1_NAME "WBase"
+#define APPLICATION1_DESC "DomoChip Wireless Izar"
+#define APPLICATION1_CLASS MyApplication
+
+#define VERSION_NUMBER "3.X"
+
+#define DEFAULT_AP_SSID "WirelessIzar"
+#define DEFAULT_AP_PSK "PasswordIzar"
+
+//Enable status webpage EventSource
+#define ENABLE_STATUS_EVENTSOURCE 0
+
+//Enable developper mode (SPIFFS editor)
+#define DEVELOPPER_MODE 0
+
+//Log Serial Object
+#define LOG_SERIAL Serial
+//Choose Log Serial Speed
+#define LOG_SERIAL_SPEED 115200
+
+//Choose Pin used to boot in Rescue Mode
+#define RESCUE_BTN_PIN 2
+
+//Define time to wait for Rescue press (in s)
+//#define RESCUE_BUTTON_WAIT 3
+
+//Status LED
+//#define STATUS_LED_SETUP pinMode(XX, OUTPUT);pinMode(XX, OUTPUT);
+//#define STATUS_LED_OFF digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
+//#define STATUS_LED_ERROR digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
+//#define STATUS_LED_WARNING digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
+//#define STATUS_LED_GOOD digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
+
+//construct Version text
+#if DEVELOPPER_MODE
+#define VERSION VERSION_NUMBER "-DEV"
+#else
+#define VERSION VERSION_NUMBER
+#endif
+
+#endif
+
+
---
+++ src/WirelessIzar.h
@@ -0,0 +1,39 @@
+#ifndef WirelessIzar_h
+#define WirelessIzar_h
+
+#include "Main.h"
+#include "base\Utils.h"
+#include "base\MQTTMan.h"
+#include "base\Application.h"
+
+const char appDataPredefPassword[] PROGMEM = "ewcXoCt4HHjZUvY1";
+
+#include "data\status1.html.gz.h"
+#include "data\config1.html.gz.h"
+
+//TODO : Rename MyApplication
+class MyApplication : public Application
+{
+private:
+  //TODO : Declare configuration properies
+
+  //TODO : Declare run/status properties
+
+  //TODO : Declare required private methods
+
+  void setConfigDefaultValues();
+  void parseConfigJSON(DynamicJsonDocument &doc);
+  bool parseConfigWebRequest(AsyncWebServerRequest *request);
+  String generateConfigJSON(bool forSaveFile);
+  String generateStatusJSON();
+  bool appInit(bool reInit);
+  const uint8_t* getHTMLContent(WebPageForPlaceHolder wp);
+  size_t getHTMLContentSize(WebPageForPlaceHolder wp);
+  void appInitWebServer(AsyncWebServer &server, bool &shouldReboot, bool &pauseApplication);
+  void appRun();
+
+public:
+  MyApplication(char appId, String fileName);
+};
+
+#endif
