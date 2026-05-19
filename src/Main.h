a784872a1bbc3b4d0db8cf70ac571f784bf450b5
DomoChip <domochip@hotmail.com>
2026-05-19 10:36:55 +0200

    Adjust for Base 2.7.7 part 1

--- src/Main.h
+++ src/Main.h
@@ -1,47 +1,62 @@
 #ifndef Main_h
 #define Main_h
 
-#include <arduino.h>
+#include <Arduino.h>
 
-//DomoChip Informations
-//Configuration Web Pages :
-//http://IP/
+// Helper macros to convert a define to a string
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
 
-#define APPLICATION1_HEADER "WirelessIzar.h"
-#define APPLICATION1_NAME "WBase"
-#define APPLICATION1_DESC "DomoChip Wireless Izar"
-#define APPLICATION1_CLASS WIzar
+// ----- Should be modified for your application -----
+#define CUSTOM_APP_MANUFACTURER "Domochip" // also used as Github user for OTA
+#define CUSTOM_APP_MODEL "WIzar"           // also used as Github repository for OTA
+#define CUSTOM_APP_CLASS WIzar
+#define VERSION_NUMBER "0.1.0"
 
-#define VERSION_NUMBER "3.0"
+#define CUSTOM_APP_HEADER TOSTRING(CUSTOM_APP_CLASS.h) // calculated header file "{CUSTOM_APP_CLASS}.h"
+#define DEFAULT_AP_SSID CUSTOM_APP_MODEL               // Default Access Point SSID "{CUSTOM_APP_MODEL}"
+#define DEFAULT_AP_PSK "password"                      // Default Access Point Password
 
-#define DEFAULT_AP_SSID "WirelessIzar"
-#define DEFAULT_AP_PSK "PasswordIzar"
+// Control EventSourceMan code (To be used by Application if EventSource server is needed)
+#define EVTSRC_ENABLED 0
+#define EVTSRC_MAX_CLIENTS 2
+#define EVTSRC_KEEPALIVE_ENABLED 1
 
-//Enable status webpage EventSource
-#define ENABLE_STATUS_EVENTSOURCE 0
-
-//Enable developper mode (SPIFFS editor)
+// Enable developper mode
 #define DEVELOPPER_MODE 0
 
-//Log Serial Object
+// Log Serial Object
 #define LOG_SERIAL Serial
-//Choose Log Serial Speed
+// Choose Log Serial Speed
 #define LOG_SERIAL_SPEED 115200
 
-//Choose Pin used to boot in Rescue Mode
+// Log Serial Macros
+#ifdef LOG_SERIAL
+#define LOG_SERIAL_PRINT(...) LOG_SERIAL.print(__VA_ARGS__)
+#define LOG_SERIAL_PRINTLN(...) LOG_SERIAL.println(__VA_ARGS__)
+#define LOG_SERIAL_PRINTF(...) LOG_SERIAL.printf(__VA_ARGS__)
+#define LOG_SERIAL_PRINTF_P(...) LOG_SERIAL.printf_P(__VA_ARGS__)
+#else
+#define LOG_SERIAL_PRINT(...)
+#define LOG_SERIAL_PRINTLN(...)
+#define LOG_SERIAL_PRINTF(...)
+#define LOG_SERIAL_PRINTF_P(...)
+#endif
+
+// Choose Pin used to boot in Rescue Mode
 // #define RESCUE_BTN_PIN 2
 
-//Define time to wait for Rescue press (in s)
-//#define RESCUE_BUTTON_WAIT 3
+// Define time to wait for Rescue press (in s)
+// #define RESCUE_BUTTON_WAIT 3
 
-//Status LED
-//#define STATUS_LED_SETUP pinMode(XX, OUTPUT);pinMode(XX, OUTPUT);
-//#define STATUS_LED_OFF digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
-//#define STATUS_LED_ERROR digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
-//#define STATUS_LED_WARNING digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
-//#define STATUS_LED_GOOD digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
+// Status LED
+// #define STATUS_LED_SETUP pinMode(XX, OUTPUT);pinMode(XX, OUTPUT);
+// #define STATUS_LED_OFF digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
+// #define STATUS_LED_ERROR digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
+// #define STATUS_LED_WARNING digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
+// #define STATUS_LED_GOOD digitalWrite(XX, HIGH);digitalWrite(XX, HIGH);
 
-//construct Version text
+// construct Version text
 #if DEVELOPPER_MODE
 #define VERSION VERSION_NUMBER "-DEV"
 #else
@@ -49,5 +64,3 @@
 #endif
 
 #endif
-
-
